a5ca206abae8c7975441291c836c1fbc7c3e1575
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-08-22 02:20:49 +0600

    penguin fashion website design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        'primary-color':'#FFFBF0',
+        'color-text':'#FABE4C',
+        'section-color':'#363958'
+      }
+    },
+  },
+  plugins: [],
+}
+
